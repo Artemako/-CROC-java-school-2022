8713bc04aff95fd19e1d0225eb0fe981a314b033
Artem <artemakopian2003@yandex.ru>
2022-10-25 20:50:42 +0300

    Окончание урока №2.

--- ru/croc/lessons/lesson2/Example1.java
+++ ru/croc/lessons/lesson2/Example1.java
@@ -24,12 +24,15 @@ public class Example1 {
         final double PI = 3.14159;
         // PI = 3.14; // error
 
+        /*
         class MyDate {
             // fields and methods
             int day = 1;
             int month = 1;
             int year;
         }
+
+         */
 
         // иденфикатор (название переменной, класса или метода) может начинаться с latin, _, $
 
@@ -54,6 +57,61 @@ public class Example1 {
          */
 
         // boolean truth = true;
+
+        /*
+        char = '\t';
+         */
+
+        /*
+        String example = """
+                Text
+                    with
+                        line breaks
+                """
+         */
+
+        /*
+        int i = 1;
+        byte b = (byte) i;
+
+        byte bb = 1;
+        int ii = bb;
+         */
+
+        /*
+        class MyDate {
+            // fields and methods
+            private int day = 1;
+            private int month = 1;
+            private int year = 2000;
+            public MyDate(int day, int month, int year) {...}
+        }
+        MyDate today = new MyDate(14, 10, 2022);
+         */
+
+        /*
+        int x = 1;
+        int y = x++;
+        System.out.println(y);
+
+         */
+
+        // System.out.println(-30%4);
+        /* int x = 1 > 2 ? 5 : 7;
+        System.out.println(x);
+        */
+
+        /*
+        double d = 7d / 3;
+        System.out.println(d);
+        d *= 100;
+        System.out.println(d);
+        d = (int) d;
+        d = (double) d;
+        d *= 0.01;
+        System.out.println(d);
+
+         */
 
     }
 }
