08bed3030a1d5b9686a147088a5f206c88e86212
Artem <artemakopian2003@yandex.ru>
2022-10-25 19:08:05 +0300

    Работаю на уроке №2.

--- ru/croc/lessons/lesson2/Example1.java
+++ ru/croc/lessons/lesson2/Example1.java
@@ -1,4 +1,59 @@
 package ru.croc.lessons.lesson2;
 
 public class Example1 {
+    public static void main(String[] args) {
+        // comment on one line
+
+        /*
+        System.out.println("Hello, Java World!");
+         */
+
+        /**
+         * Документация
+         * @param;
+         * @return;
+         */
+
+        /*
+        int x;
+        x = 1;
+        int y = x + 2;
+        System.out.println(y);
+         */
+
+        final double PI = 3.14159;
+        // PI = 3.14; // error
+
+        class MyDate {
+            // fields and methods
+            int day = 1;
+            int month = 1;
+            int year;
+        }
+
+        // иденфикатор (название переменной, класса или метода) может начинаться с latin, _, $
+
+        /*
+        PascalCase - название класса JAVA
+        snake_case - название констант (верхний регистр) JAVA
+        camelCase - название переменной JAVA
+         */
+
+        /*
+        int x = 2021; // 10-ричная
+        x = 2_021; // 10-ричная
+        x = 03745; // 8-ричная
+        x = 0x7E5; // 16-ричная
+        x = 0b0111_1110_0101; // 2-ричная
+         */
+
+        /*
+        double d = 3.14;
+        float f = 3.14f;
+        f = 2.15f;
+         */
+
+        // boolean truth = true;
+
+    }
 }
